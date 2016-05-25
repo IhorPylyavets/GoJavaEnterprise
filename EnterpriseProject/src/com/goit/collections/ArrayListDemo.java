af1baf0f10e0ebe2c7fbfc26c31b1acfdbeb96fd
i.pylyavets <ihor.pylyavets@gmail.com>
2016-05-25 12:10:41 +0300

    added ArrayList.get()

--- EnterpriseProject/src/com/goit/collections/ArrayListDemo.java
+++ EnterpriseProject/src/com/goit/collections/ArrayListDemo.java
@@ -11,8 +11,11 @@ public class ArrayListDemo {
 
     public static void main(String[] args) {
         long[] averageAddTimes = getArrayTimeResultArrayListAdd();
+        long[] averageGetTimes = getArrayTimeResultArrayListGet();
 
         System.out.println("Test ArrayList.add(): " + Arrays.toString(averageAddTimes));
+        System.out.println("Test ArrayList.get(): " + Arrays.toString(averageGetTimes));
+
     }
 
     private static long[] getArrayTimeResultArrayListAdd() {
@@ -58,5 +61,51 @@ public class ArrayListDemo {
         }
 
         return  resultTime/timeArray.length;
+    }
+
+    private static long[] getArrayTimeResultArrayListGet() {
+        long[] averageTimes = new long[3];
+
+        long[] array10k = new long[COUNT_OPERATIONS];
+        long[] array100k = new long[COUNT_OPERATIONS];
+        long[] array1000k = new long[COUNT_OPERATIONS];
+
+        for (int i = 0; i < COUNT_OPERATIONS; i++) {
+            array10k[i] = getElementToArrayList(VOLUME_10K);
+        }
+        averageTimes[0] = getAverageTime(array10k);
+
+        for (int i = 0; i < COUNT_OPERATIONS; i++) {
+            array100k[i] = getElementToArrayList(VOLUME_100K);
+        }
+        averageTimes[1] = getAverageTime(array100k);
+
+        for (int i = 0; i < COUNT_OPERATIONS; i++) {
+            array1000k[i] = getElementToArrayList(VOLUME_1000K);
+        }
+        averageTimes[2] = getAverageTime(array1000k);
+
+        return averageTimes;
+    }
+
+    private static ArrayList randomArrayListFilling(int count) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < count; i++) {
+            list.add(((int)(Math.random() * VOLUME_1000K)));
+        }
+
+        return list;
+    }
+
+    private static long getElementToArrayList(int count) {
+        ArrayList<Integer> list = randomArrayListFilling(count);
+
+        long startTime = System.nanoTime();
+        for (int i = 0; i < count; i++) {
+            int a = list.get(i);
+        }
+        long endTime = System.nanoTime();
+        return endTime - startTime;
     }
 }
