5afa9af11c8b6eff1dc04597c64800a3f56af972
ihor.pylyavets <ihor.pylyavets@gmail.com>
2016-05-25 22:41:43 +0300

    added ArrayList.remove()

--- EnterpriseProject/src/com/goit/collections/ArrayListDemo.java
+++ EnterpriseProject/src/com/goit/collections/ArrayListDemo.java
@@ -11,18 +11,21 @@ public class ArrayListDemo {
 
     public static void main(String[] args) {
         long[] averageAddTimes = getArrayTimeResultArrayListAdd();
-        long[] averageGetTimes = getArrayTimeResultArrayListGet();
-
         System.out.println("Test ArrayList.add(): " + Arrays.toString(averageAddTimes));
+
+        long[] averageGetTimes = getArrayTimeResultArrayListGet();
         System.out.println("Test ArrayList.get(): " + Arrays.toString(averageGetTimes));
+
+        long[] averageRemoveTimes = getArrayTimeResultArrayListRemove();
+        System.out.println("Test ArrayList.remove(): " + Arrays.toString(averageRemoveTimes));
 
     }
 
     private static long[] getArrayTimeResultArrayListAdd() {
         long[] averageTimes = new long[3];
 
-        long[] array10k = new long[COUNT_OPERATIONS];
-        long[] array100k = new long[COUNT_OPERATIONS];
+        long[] array10k   = new long[COUNT_OPERATIONS];
+        long[] array100k  = new long[COUNT_OPERATIONS];
         long[] array1000k = new long[COUNT_OPERATIONS];
 
         for (int i = 0; i < COUNT_OPERATIONS; i++) {
@@ -45,9 +48,9 @@ public class ArrayListDemo {
 
     private static long addElementToArrayList(int count) {
         long startTime = System.nanoTime();
-        ArrayList<Integer> list = new ArrayList<>();
+        ArrayList<Integer> elements = new ArrayList<>();
         for (int i = 0; i < count; i++) {
-            list.add(i);
+            elements.add(i);
         }
         long endTime = System.nanoTime();
         return endTime - startTime;
@@ -66,22 +69,22 @@ public class ArrayListDemo {
     private static long[] getArrayTimeResultArrayListGet() {
         long[] averageTimes = new long[3];
 
-        long[] array10k = new long[COUNT_OPERATIONS];
-        long[] array100k = new long[COUNT_OPERATIONS];
+        long[] array10k   = new long[COUNT_OPERATIONS];
+        long[] array100k  = new long[COUNT_OPERATIONS];
         long[] array1000k = new long[COUNT_OPERATIONS];
 
         for (int i = 0; i < COUNT_OPERATIONS; i++) {
-            array10k[i] = getElementToArrayList(VOLUME_10K);
+            array10k[i] = getElementFromArrayList(VOLUME_10K);
         }
         averageTimes[0] = getAverageTime(array10k);
 
         for (int i = 0; i < COUNT_OPERATIONS; i++) {
-            array100k[i] = getElementToArrayList(VOLUME_100K);
+            array100k[i] = getElementFromArrayList(VOLUME_100K);
         }
         averageTimes[1] = getAverageTime(array100k);
 
         for (int i = 0; i < COUNT_OPERATIONS; i++) {
-            array1000k[i] = getElementToArrayList(VOLUME_1000K);
+            array1000k[i] = getElementFromArrayList(VOLUME_1000K);
         }
         averageTimes[2] = getAverageTime(array1000k);
 
@@ -98,14 +101,51 @@ public class ArrayListDemo {
         return list;
     }
 
-    private static long getElementToArrayList(int count) {
+    private static long getElementFromArrayList(int count) {
         ArrayList<Integer> list = randomArrayListFilling(count);
 
         long startTime = System.nanoTime();
         for (int i = 0; i < count; i++) {
-            int a = list.get(i);
+            int tempElement = list.get(i);
         }
         long endTime = System.nanoTime();
         return endTime - startTime;
+    }
+
+    private static long removeElementFromArrayList(int count) {
+        ArrayList<Integer> list = randomArrayListFilling(count);
+
+        long startTime = System.nanoTime();
+        for (int i = count-1; i > 0; i--) {
+            int res = list.remove(i);
+        }
+        long endTime = System.nanoTime();
+
+        return endTime - startTime;
+    }
+
+    private static long[] getArrayTimeResultArrayListRemove() {
+        long[] averageTimes = new long[3];
+
+        long[] array10k   = new long[COUNT_OPERATIONS];
+        long[] array100k  = new long[COUNT_OPERATIONS];
+        long[] array1000k = new long[COUNT_OPERATIONS];
+
+        for (int i = 0; i < COUNT_OPERATIONS; i++) {
+            array10k[i] = removeElementFromArrayList(VOLUME_10K);
+        }
+        averageTimes[0] = getAverageTime(array10k);
+
+        for (int i = 0; i < COUNT_OPERATIONS; i++) {
+            array100k[i] = removeElementFromArrayList(VOLUME_100K);
+        }
+        averageTimes[1] = getAverageTime(array100k);
+
+        for (int i = 0; i < COUNT_OPERATIONS; i++) {
+            array1000k[i] = removeElementFromArrayList(VOLUME_1000K);
+        }
+        averageTimes[2] = getAverageTime(array1000k);
+
+        return averageTimes;
     }
 }
