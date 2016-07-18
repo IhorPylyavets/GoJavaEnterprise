c9d38f54f243363240e71fcffdc72ae442ead1ba
i.pylyavets <ihor.pylyavets@gmail.com>
2016-07-18 18:28:43 +0300

    added aop logget with interceptor

---
+++ startSpring/MavenExample/src/main/java/com/goit/ExecutorInterceptor.java
@@ -0,0 +1,15 @@
+package com.goit;
+
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+
+public class ExecutorInterceptor implements MethodInterceptor {
+
+    @Override
+    public Object invoke(MethodInvocation invocation) throws Throwable {
+        System.out.println("Interceptor. Before execution of: " + invocation.getMethod().getName());
+        Object result = invocation.proceed();
+        System.out.println("Interceptor. After execution of:  " + invocation.getMethod().getName());
+        return result;
+    }
+}
