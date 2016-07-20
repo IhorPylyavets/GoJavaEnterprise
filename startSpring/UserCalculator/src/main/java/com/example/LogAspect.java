d04fc235504c58b997c53129d78c9a9fde92b19c
i.pylyavets <ihor.pylyavets@gmail.com>
2016-07-20 18:29:04 +0300

    added log4j

--- startSpring/UserCalculator/src/main/java/com/example/LogAspect.java
+++ startSpring/UserCalculator/src/main/java/com/example/LogAspect.java
@@ -1,17 +1,26 @@
 package com.example;
 
+import org.apache.log4j.Logger;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 
 @Aspect
 public class LogAspect {
 
-    @Around("execution( * com.goit.springproject.arithmetic.Calculator.execute(String))")
+    final static Logger logger = Logger.getLogger(LogAspect.class);
+
+    /*@Around("execution( * com.goit.springproject.arithmetic.Calculator.execute(String))")
     public Object onExecute(ProceedingJoinPoint pjp) throws Throwable {
         System.out.println("LogAspect. Before execution of: " + pjp.getSignature().getName());
         Object result = pjp.proceed();
         System.out.println("LogAspect. After execution of:  " + pjp.getSignature().getName());
         return result;
+    }*/
+
+    @Before("execution( * com.goit.springproject.arithmetic.Calculator.execute(String)) && args(expressions)")
+    public void onExecute(String expressions) {
+        logger.info("Calculator: Expression: " + expressions);
     }
 }
