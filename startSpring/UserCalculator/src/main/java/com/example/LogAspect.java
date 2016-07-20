91498bc749a83ffbe265a401cd99770170a7e5df
i.pylyavets <ihor.pylyavets@gmail.com>
2016-07-20 17:43:53 +0300

    fix to annotation log

--- startSpring/UserCalculator/src/main/java/com/example/LogAspect.java
+++ startSpring/UserCalculator/src/main/java/com/example/LogAspect.java
@@ -1,9 +1,13 @@
 package com.example;
 
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
 
+@Aspect
 public class LogAspect {
 
+    @Around("execution( * com.goit.springproject.arithmetic.Calculator.execute(String))")
     public Object onExecute(ProceedingJoinPoint pjp) throws Throwable {
         System.out.println("LogAspect. Before execution of: " + pjp.getSignature().getName());
         Object result = pjp.proceed();
