86a39ce967af9e7ab52c4a1d6cc45b294f288cf2
i.pylyavets <ihor.pylyavets@gmail.com>
2016-09-21 00:09:04 +0300

    start rest

--- mvc/Spring-mvc-example/src/main/java/com/example/web/EmployeeController.java
+++ mvc/Spring-mvc-example/src/main/java/com/example/web/EmployeeController.java
@@ -4,23 +4,21 @@ import com.example.model.Employee;
 import com.example.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 import java.util.Map;
 
-//@Controller
+/*@Controller
+@ResponseBody*/
 @RestController
 public class EmployeeController {
 
     private EmployeeService employeeService;
 
     @RequestMapping(value = "/employee", method = RequestMethod.GET)
-    public List<Employee> employee(Map<String, Object> model) {
+    public List<Employee> employees(Map<String, Object> model) {
         return employeeService.getEmployees();
     }
 
